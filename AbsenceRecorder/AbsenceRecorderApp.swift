6b17fc677eef116d39c2b7a776c19d5230bb0fd7
Zhenglong Wu <wu.z@etoncollege.org.uk>
2021-02-28 14:43:38 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Zhenglong Wu on 28/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbsenceRecorder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AbsenceRecorder
+//
+//  Created by Zhenglong Wu on 28/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
